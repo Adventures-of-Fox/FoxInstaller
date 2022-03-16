b9e00419d5132336655d9c31a0d1d8b270c3e1e2
JustFoxx <justfoxxo@outlook.com>
2022-03-16 19:26:12 +0100

    Added readme, Added checking, Added error function

---
+++ cdn/README.txt
@@ -0,0 +1,22 @@
+WELCOME TO README!
+If you have any problem, check out https://github.com/Adventures-of-Fox/FoxInstaller.
+
+--- Info ---
+    1. Minecraft server requires java 17
+    2. Minecraft server requires minimal 4GB of ram (6GB IS RECOMMENDED) (8GB SHOULD BE MAX)
+    3. Installer is made only for Linux and Window
+    4. You need manually update modpack
+--- Info ---
+
+--- Linux ---
+    1. Run linux.sh (if you cant run, type `chmod +x`)
+    2. Wait to moment everything will be done
+    3. Then run start.sh (edit RAM_MAX and RAM_MIN variables to change ram for server)
+    4. Accept EULA
+    5. Run again
+    6. Done!
+--- Linux ---
+
+--- Windows ---
+[TODO]
+--- Windows ---
--- src/linux.sh
+++ src/linux.sh
@@ -4,39 +4,36 @@ bad=none
 function color() {
     echo -e "$1$2\033[0m"
 }
+function error() {
+    color "\033[1;31m" "    ✗ $1 - $2"
+    color "\033[1;31m" "✗ $3         - Error in $1"
+    exit 1
+}
 
 color "\033[1;37m" "         STARTING SCRIPT"
-echo 
+echo
 
 #--- Checking ---
 color "\033[1;33m" "🗘 Checking         - (Tar,Curl,Java,Java version,Internet)"
+main="Checking        "
 
 #Check tar
-if ! [ -x "$(command -v tar)" ]; then
-    color "\033[1;31m" "    ✗ Tar          - Tar doesn't exist on your pc."
-    bad="Curl"
-    color "\033[1;31m" "✗ Checking         - Error in $bad"
-    exit 1
+if [ -x "$(command -v tar)" ]; then
+    error "Tar         " "Java doesn't exist on your pc." "$main"
 else
     color "\033[1;32m" "    ✓ Tar          - OK"
 fi
 
 #Check curl
 if ! [ -x "$(command -v curl)" ]; then
-    color "\033[1;31m" "    ✗ Curl         - Curl doesn't exist on your pc."
-    bad="Curl"
-    color "\033[1;31m" "✗ Checking         - Error in $bad"
-    exit 1
+    error "Curl        " "Java doesn't exist on your pc." "$main"
 else
     color "\033[1;32m" "    ✓ Curl         - OK"
 fi
 
 #Check java
 if ! [ -x "$(command -v java)" ]; then
-    color "\033[1;31m" "    ✗ Java         - Java doesn't exist on your pc."
-    bad="Java"
-    color "\033[1;31m" "✗ Checking         - Error in $bad"
-    exit 1
+    error "Java        " "Java doesn't exist on your pc." "$main"
 else
     color "\033[1;32m" "    ✓ Java         - OK"
 fi
@@ -45,20 +42,14 @@ fi
 _java=java
 version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
 if ! [[ "$version" > "17.0" ]]; then
-    color "\033[1;31m" "    ✗ Java version - Your java version ($version) need be 17.0 or newer"
-    bad="Java version"
-    color "\033[1;31m" "✗ Checking         - Error in $bad"
-    exit 1
+    error "Java version    " "Your java version ($version) need be 17.0 or newer" "$main"
 else
     color "\033[1;32m" "    ✓ Java version - OK"
 fi
 
 #Check internet
-if ! ping -q -w 1 -c 1 google.com > /dev/null ; then
-    color "\033[1;31m" "    ✗ Internet     - No internet connection"
-    bad="Internet"
-    color "\033[1;31m" "✗ Checking         - Error in $bad"
-    exit 1
+if ! ping -q -w 1 -c 1 google.com >/dev/null; then
+    error "Internet    " "No internet connection" "$main"
 else
     color "\033[1;32m" "    ✓ Internet     - OK"
 fi
@@ -70,38 +61,30 @@ echo
 
 #--- Downloading ---
 color "\033[1;33m" "🗘 Downloading      - (Version,Start script,Server jar,Mods)"
+main="Downloading     "
 
 #Version
-if ! curl -s 'https://raw.githubusercontent.com/Adventures-of-Fox/FoxInstaller/main/cdn/version' > ".version"; then
-    color "\033[1;31m" "    ✗ Version      - Error failed"
-    bad="Version"
-    color "\033[1;31m" "✗ Downloading      - Error in $bad"
-    exit 1
+if ! curl -s 'https://raw.githubusercontent.com/Adventures-of-Fox/FoxInstaller/main/cdn/version' >".version"; then
+    error "Version     " "Error failed download" "$main"
 else
     color "\033[1;32m" "    ✓ Version      - OK"
 fi
 
 #Start script
-if ! curl -s 'https://raw.githubusercontent.com/Adventures-of-Fox/FoxInstaller/main/cdn/start-linux.sh' > "start.sh"; then
-    color "\033[1;31m" "    ✗ Start script - Error failed"
-    bad="Start script"
-    color "\033[1;31m" "✗ Downloading      - Error in $bad"
-    exit 1
+if ! curl -s 'https://raw.githubusercontent.com/Adventures-of-Fox/FoxInstaller/main/cdn/start-linux.sh' >"start.sh"; then
+    error "Start script" "Error failed download" "$main"
 else
     color "\033[1;32m" "    ✓ Start script - OK"
 fi
 
 #Server jar
-if ! curl -s 'https://meta.fabricmc.net/v2/versions/loader/1.18.1/0.13.3/0.10.2/server/jar' > "server.jar"; then
-    color "\033[1;31m" "    ✗ Server jar   - Error failed"
-    bad="Server jar"
-    color "\033[1;31m" "✗ Downloading      - Error in $bad"
-    exit 1
+if ! curl -s 'https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.2/fabric-installer-0.10.2.jar' >"installer_server.jar"; then
+    error "Server jar  " "Error failed download" "$main"
 else
     color "\033[1;32m" "    ✓ Server jar   - OK"
 fi
 
-#Mods
+# Mods
 # if ! curl -s 'example.com' > "."; then
 #     color "\033[1;31m" "    ✗ Mods         - Error failed"
 #     bad="Server jar"
@@ -110,3 +93,30 @@ fi
 # else
 #     color "\033[1;32m" "    ✓ Mods         - OK"
 # fi
+
+color "\033[1;32m" "✓ Checking      - OK"
+#--- Downloading ---
+
+echo
+
+#--- Preparing ---
+main="Preparing     "
+color "\033[1;33m" "🗘 Preparing      - (Downloading server files)"
+
+# Server files
+if ! java -jar installer_server.jar server -mcversion 1.18.1 -downloadMinecraft; then
+    error "\033[1;31m" "    ✗ Server files - Error failed" "$main"
+else
+    color "\033[1;32m" "    ✓ Server files - OK"
+fi
+rm installer_server.jar
+
+color "\033[1;32m" "✓ Preparing      - OK"
+#--- Preparing ---
+
+echo
+
+#--- END ---
+color "\033[1;37m" "         RUN start.sh TO RUN SERVER"
+color "\033[1;37m" "         CHECK README.TXT"
+color "\033[1;37m" "         END SCRIPT"
